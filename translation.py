9ce8b913d3824ea034475a94b6867c729518fb93
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2020-06-30 08:33:57 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -5,11 +5,9 @@ I am Telegram Rename Bot!
 I can Rename all type of telegram files with Permanent Custom Thumbnail Support🥳
 
 
-<b>Please send me any Telegram file and reply /rename New Name.extn</b>
-
 See /help For My Features
 
-🚨 <code>Please do one by one Rename,Maximum 2 parallel process allowed.If you missuse this free service will be lead you PERMANENT BAN </code> 🚨
+🚨 <code>Please do one by one Rename,Maximum 2 parallel process allowed.If you missuse this free service will lead you PERMANENT BAN </code> 🚨
 
 Created by This <a href="https://telegram.dog/oo7robot">Person</a>"""
 
