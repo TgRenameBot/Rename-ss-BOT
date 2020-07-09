6db604030db228e186707bc76408e049082d17c6
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2020-07-09 23:31:45 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -42,12 +42,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "\nDownloading : {2}%   of 100 \n\n{0}{1} \n\n".format(
+        progress = "\nDownloading.... {2}%  \n\n{0}{1} \n\n".format(
             ''.join(["🟩" for i in range(math.floor(percentage / 10))]),
             ''.join(["⬜️" for i in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2))
 
-        tmp = progress + "\n 📥 <b>Downloaded</b>   : `{0}`\n\n🗂️ <b>Total Size</b>       : `{1}` \n\n🚀 <b>Speed</b>             : `{2}/s`\n\n⏳ <b>Time Left</b>        : `{3}`\n\n\n`Kindly Wait for Finish this Renaming Process then Send Another FILE` \n\n <b>Thanks For Using @TgRenamebot</b>\n           <b>MyTestBotZ</b>".format(
+        tmp = progress + "🗂️ <b>Total File Size</b> : `{1}`\n\n📥 <b>Downloaded</b>.....: `{0}` \n🚀 <b>Speed</b>...........: `{2}/s`\n⏳ <b>Time Left</b>......: `{3}`\n\n\n`Kindly Wait for Finish this Renaming Process then Send Another FILE` \n\n <b>Thanks For Using @TgRenamebot</b>\n ┈┈┈••✿ <b>@MyTestBotZ</b> ✿••┈┈┈".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
